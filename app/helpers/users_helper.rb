367471d292e36437ecc9a1ca929ce6c055e04cb1
snakedoc23 <snakedoc23@gmail.com>
2011-09-09 13:02:46 +0200

    Added some style

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,9 +2,9 @@ module UsersHelper
   
   def user_avatar(user, size = "100x100")
     if user.avatar_url
-      image_tag "/assets/avatar-#{user.id}.jpg", :size => size, :alt => user.username
+      image_tag "/images/avatar-#{user.id}.jpg", :size => size, :alt => user.username
     else
-      image_tag "/assets/avatar-default.png", :size => size, :alt => user.username
+      image_tag "/images/avatar-default.png", :size => size, :alt => user.username
     end
   end
   
